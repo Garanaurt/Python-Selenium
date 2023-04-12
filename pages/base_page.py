165d81a1648bce4a18b3b8022952f6e215eca1e2
Garanaurt <bnotchenko@gmail.com>
2023-04-12 19:20:43 +0300

    Perenos v base_page togo chto est na vseh stranicah

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,14 +2,22 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 class BasePage():
     def __init__(self, browser, url, timeout=10): #запуск и ожидание
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()  
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+    
     def is_element_present(self, how, what): #проверка есть ли на странице элемент
         try:
             self.browser.find_element(how, what)
--- test_product_page.py
+++ test_product_page.py
@@ -21,23 +21,35 @@ def atest_guest_can_add_product_to_basket(browser, link):
     page.is_price_added_prod_valid()
 
 @pytest.mark.xfail(reason="элемент должен быть, падает специально")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+def atest_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser):
+def atest_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
 @pytest.mark.xfail(reason="падает специально")
-def test_message_disappeared_after_adding_product_to_basket(browser):
+def atest_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.should_be_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
